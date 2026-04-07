d91f7d8b770274a8f03e91ff1bbcf5ce0a33be93
Carlos <carloshenriquepey2025@gmail.com>
2026-04-07 20:57:46 -0300

    Django criar

--- app_ControleDeTarefas/urls.py
+++ app_ControleDeTarefas/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from app_ControleDeTarefas.views import index, tarefas
+from app_ControleDeTarefas.views import index, tarefas, criar
 
 urlpatterns = [
 path('', index, name='index'),
-path('tarefas/', tarefas, name='tarefas')
+path('tarefas/', tarefas, name='tarefas'),
+path('criar/', criar, name="criar")
 ]
 
--- app_ControleDeTarefas/views.py
+++ app_ControleDeTarefas/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from app_ControleDeTarefas.models import Tarefa
 
 def index(request):
     return render(request,'app_ControleDeTarefas/index.html')
 
 def tarefas(request):
-    return render(request,'app_ControleDeTarefas/ControleDeTarefas.html')
+    banco = Tarefa.objects.all()
+    return render(request,'app_ControleDeTarefas/ControleDeTarefas.html', {'banco': banco })
+
+def criar(request):
+    if request.method == 'POST':
+        titulo = request.POST.get('titulo')
+        descricao = request.POST.get('descricao')
+        data = request.POST.get('data')
+        status = False
+        Tarefa.objects.create(titulo=titulo, descricao=descricao, data=data, status=status)
+        return redirect('tarefas')
+    return render(request, 'app_ControleDeTarefas/criar.html')
