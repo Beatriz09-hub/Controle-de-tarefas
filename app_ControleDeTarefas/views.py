057fa9d939a26245c46d6574e7a6b442a7f5ecf2
Beatriz09-hub <beatrizlimeira70@gmail.com>
2026-05-08 20:40:12 -0300

    Impedindo Invasões

--- app_ControleDeTarefas/views.py
+++ app_ControleDeTarefas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import  get_object_or_404, redirect, render
 from app_ControleDeTarefas.forms import TarefaForm
 from app_ControleDeTarefas.models import Tarefa
 from django.contrib.auth.decorators import login_required
@@ -18,7 +18,8 @@ def criar(request):
     return render(request, 'app_ControleDeTarefas/criar.html', {'form' : form})
 @login_required
 def excluir(request,id):
-    tarefa = Tarefa.objects.get(id=id)
+    # tarefa = Tarefa.objects.get(id=id)
+    tarefa = get_object_or_404(Tarefa, id=id, usuario = request.user)
     if request.method == 'POST':
         tarefa.delete()
         return redirect('tarefas')
@@ -26,7 +27,8 @@ def excluir(request,id):
 
 def editar(request, id):
 
-    tarefa = Tarefa.objects.get(id=id)
+    # tarefa = Tarefa.objects.get(id=id)
+    tarefa = get_object_or_404(Tarefa, id=id, usuario = request.user)
     form = TarefaForm(request.POST or None, instance=tarefa)
     if request.method == 'POST':
         if form.is_valid():
